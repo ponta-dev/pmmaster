060cc832c01eabe1db46ee798c43e55ec859a8bc
ponta-dev <ponta.dev.matumoto@gmail.com>
2022-10-25 00:03:44 +0900

    初期構築

--- pmmaster-ui-angular/src/app/app-routing.module.ts
+++ pmmaster-ui-angular/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TopComponent } from './top/component/top/top.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/top', pathMatch: 'full' }
+  ,{ path: 'top', component: TopComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ pmmaster-ui-angular/src/app/top/top.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TopComponent } from './component/top/top.component';
+
+
+
+@NgModule({
+  declarations: [
+    TopComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TopModule { }
